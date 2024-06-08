25c0cacc4668d1cc8c1a75a76a790d24b21cad05
audinex <124537555+audinex@users.noreply.github.com>
2024-06-07 23:04:32 -0300

    Add files via upload

---
+++ lista 2/exercicio 5 da lista 2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	float larg;
+	float comp;
+	
+	cout<< "insira a largura e o comprimento da sala em metros: " << endl;
+	cin>> larg;
+	cin>> comp;
+	
+	cout<< "a area da sua sala: " << larg*comp << " metros quadrados";
+}
