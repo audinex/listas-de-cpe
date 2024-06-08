971d41dd25f59f037e3f954f8cca638d7fe3c8ab
audinex <124537555+audinex@users.noreply.github.com>
2024-06-07 23:18:02 -0300

    Add files via upload

---
+++ lista 4/letra c questao 1 lista 4.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cmath> 
+#include <iomanip>
+using namespace std;
+
+double fatorial(int n) {
+    double resultado = 1;
+    for (int i = 2; i <= n; ++i) {
+        resultado *= i;
+    }
+    return resultado;
+}
+
+double calculadora(double x, int termos) {
+    double operador1 = 1.0;
+    double operador2 = 1.0;
+    for (int i = 1; i <= termos; ++i) {
+        operador2 *= x;
+        operador1 += operador2 / fatorial(i);
+    }
+    return operador1;
+}
+
+int main() {
+    double x;
+    int termos;
+
+    cout << "Digite o valor de x para calcular ex: ";
+    cin >> x;
+
+    cout << "Digite o número de termos para a série de Taylor: ";
+    cin >> termos;
+
+    cout << fixed << setprecision(10);
+
+    double operador1 = calculadora(x, termos);
+    cout << "O valor de e^" << x << " com " << termos << " termos é: " << operador1 << endl;
+
+    return 0;
+}
